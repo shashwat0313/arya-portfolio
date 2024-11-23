573fc73ea01b514fca4c1f1e57d04d6c77188fb7
shashwat0313 <shashwat0313@yahoo.co.in>
2024-11-24 02:29:57 +0530

    misc

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -1,15 +1,21 @@
 import BoldHeading from "./text/BoldHeading";
 import Heading1 from "./text/Heading1";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
     return (
+        <>
         <div className="text-center">
+            <Link to="/">
+            <div className=" font-piazzolla text-green-600 text-base font-bold underline">Back to Homepage</div>
+            </Link>
             <br></br>
             <br></br>
             <Heading1 text="Contact Info:"/>
             <BoldHeading text="Email:"/>
             <BoldHeading text="aryashrestha105@gmail.com"/>
         </div>
+        </>
     );
 };
 

08e6f184028d8900d3edffc9f11fb92f1602407a
shashwat0313 <shashwat0313@yahoo.co.in>
2024-11-24 16:28:39 +0530

    added all pictures

--- portfolio/src/components/Certifications.jsx
+++ portfolio/src/components/Certifications.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import Heading1 from './text/Heading1';
 
 const Certifications = () => {
     return (
-        <div>
-            <h2>Certifications</h2>
-            <ul>
-                <li>Certification 1</li>
-                <li>Certification 2</li>
-                <li>Certification 3</li>
-            </ul>
+        <div className='mx-4'>
+            <Heading1 text='Volunteering Work' />
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                <div className="relative overflow-hidden">
+                    <img src="img1.jpg" alt="Description 1" className="object-contain transform transition-transform duration-500 hover:scale-110" />
+                </div>
+                <div className="relative overflow-hidden">
+                    <img src="img2.jpg" alt="Description 2" className="object-contain transform transition-transform duration-500 hover:scale-110" />
+                </div>
+                <div className="relative overflow-hidden">
+                    <img src="img3.jpg" alt="Description 3" className="object-contain transform transition-transform duration-500 hover:scale-110" />
+                </div>
+            </div>
+            <Heading1 text='Certificates' />
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                <div className="relative overflow-hidden ">
+                    {/* <img src="img4.jpg" alt="Description 1" className="object-contain transform transition-transform duration-500 hover:scale-110" /> */}
+                </div>
+                <div className="relative overflow-hidden ">
+                    <img src="img4.jpg" alt="Description 1" className="object-contain transform transition-transform duration-500 hover:scale-110" />
+                </div>
+            </div>
+
         </div>
     );
 };

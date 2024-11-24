a72d8a41f0159a3d17a3bff4ea83354928218176
shashwat0313 <shashwat0313@yahoo.co.in>
2024-11-24 16:04:44 +0530

    added homepage picture

--- portfolio/src/components/HomeContent.jsx
+++ portfolio/src/components/HomeContent.jsx
@@ -2,10 +2,16 @@ import Heading1 from './text/Heading1';
 import TextParagraph from './text/TextParagraph';
 import BoldHeading from './text/BoldHeading';
 import TwoColumns from './TwoColumns';
+// import '../assets/homepage-img.jpg';
 
 const HomeContent = () => {
     return (
         <div className="lg-max-w">
+            <img 
+                src="homepage-img.jpg"
+                alt="Descriptive Alt Text" 
+                className="h-[48vh] lg:h-[60vh] w-full object-contain object-top"
+            />
             <Heading1 text='A quick learner and team player with great communication skills, always aiming to deliver efficient and high quality results.' />
 
             <TextParagraph text={"I’ve graduated from Bengaluru City University with a strong foundation in writing, editing, public relations, advertising, and various digital media and software."} />

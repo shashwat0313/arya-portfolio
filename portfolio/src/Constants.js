70dcaf2f11182b78d548970d3f31aa40d2c5d6fd
shashwat0313 <shashwat0313@yahoo.co.in>
2025-05-12 04:02:49 +0530

    url undo commit

--- portfolio/src/Constants.js
+++ portfolio/src/Constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL = "/arya-portfolio"
 
-export const BACKEND_BASE_URL = "http://localhost:3000"
-// export const BACKEND_BASE_URL = "https://arya-portfolio-backend-ezf8cggcb6abf0dk.centralindia-01.azurewebsites.net";
+// export const BACKEND_BASE_URL = "http://localhost:3000"
+export const BACKEND_BASE_URL = "https://arya-portfolio-backend-ezf8cggcb6abf0dk.centralindia-01.azurewebsites.net";
